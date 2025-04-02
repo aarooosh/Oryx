4130c69c6a1e01feb6d546c7dbb74e10f0e78d91
A_Ghate <98141133+aarooosh@users.noreply.github.com>
2025-04-02 17:06:59 +0530

    Update processor.v

--- processor.v
+++ processor.v
@@ -1,3 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/02/2025 02:08:30 PM
+// Design Name: 
+// Module Name: processor
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module processor(
     input clk,
     input rst
@@ -5,35 +27,58 @@ module processor(
 
     reg [31:0] pc;
     reg [31:0] ir;
+    reg [31:0] hi;
+    reg [31:0] lo;
+    reg [3:0] ALU_inst;
+    reg branch;
+    
+    //control singals
+    reg i_r;
+    reg write_reg_en;
+    reg regfile_src_oalu_st;
+    reg [31:0] temp;
+    reg jump;
 
-    regfile();
-    inst_rom(pc,ir); // this is combinational ! constant update
-    stack();
-    ALU();
+//    regfile();
+//    inst_rom(pc,ir); // this is combinational ! constant update
+//    stack();
+//    ALU();
 
-    initial begin
-        pc = 32'd0;
-    end
+//    initial begin
+//        pc = 32'd0;
+//    end
     //for now i'm doing it here instead of a separate module
     //each case will set specific control signals like branch,i/r,r/w,wr_en,ALU_go_ahead etc
-    always begin
-        case (ir[31:29])
-            3'd0 : //arithmetic
-            3'd6 : //logic gate
-            3'd7 : //shift gate
-            3'd1 : //data
-            3'd2 : //branch
-            3'd3 : //jump
-            3'd4 : //comparison
-            3'd5 : //FLOP
-        endcase
-    end
+   
     
-    always @(posedge clk) begin
-        if (rst) begin
+    always @(posedge clk) 
+    begin
+        pc = pc+32'd4;
+        if (rst) 
+        begin
             pc <= 32'd0;
         end
-        //implement only the PC update logic and writing (to mem or reg) here
+        
+        else 
+        begin
+        if (branch==1'b1 && jump==1'b0) 
+            begin
+                temp[17:2] = ir[15:0];
+                temp[1:0] = 2'b00;
+                temp[31:18] = {ir[16],ir[16],ir[16],ir[16],ir[16],ir[16],ir[16],ir[16],ir[16],ir[16],ir[16],ir[16],ir[16],ir[16]};
+                // do this using wires
+                pc = pc+temp;
+            end
+        
+        else if(jump) 
+            begin
+            if (branch == 1'b0)
+                pc[29:0] = {ir[27:0],2'b00};
+            else
+                //implement jr part
+            end
+        end
+        //implement only the PC update logic here
     end
 
 
